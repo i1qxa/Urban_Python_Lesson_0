5f99d7ef86c09a5ea1fb7467a74d75313351d39c
Иван <cvb@sdf>
2024-10-11 22:13:58 +0300

    LESSON_0_BASE_DATA_STRUCTURE

---
+++ main.py
@@ -0,0 +1,18 @@
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+firstNum = 2 * 2 + 2
+secondNum = 2 * (2 + 2)
+print(firstNum)
+print(secondNum)
+print(firstNum == secondNum)
+
+# 4th program
+numAsString = '123.456'
+numAsFloat = float(numAsString) * 10
+numAsInt = int(numAsFloat)
+print(numAsInt % 10)
