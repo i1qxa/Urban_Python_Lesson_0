78668b19b2b28ba4993ec098c46376ae8f9f3d32
Иван <cvb@sdf>
2024-10-12 23:23:46 +0300

    LESSON_1_6

---
+++ lesson_1/module_1_6.py
@@ -0,0 +1,9 @@
+number_homework_completed = 12
+number_hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = number_hours_spent / number_homework_completed
+print("Курс: ", course_name,
+      ", всего задач:", number_homework_completed,
+      ", затрачено часов: ", number_hours_spent,
+      ", среднее время выполнения ", time_for_one_task, " часа."
+      )
