afb161e64a3fb75c8ead07ffb6070283051b09bd
Иван <cvb@sdf>
2024-10-13 15:34:26 +0300

    LESSON_1_9

---
+++ lesson_1/module_1_9.py
@@ -0,0 +1,13 @@
+# task 1
+example = "Пепелац"
+# task 2
+print(example[0])
+# task 3
+print(example[-1])
+# task 4
+middle_of_string = int(example.__len__() / 2)
+print(example[middle_of_string:])
+# task 5
+print(example[::-1])
+# task 6
+print(example[1::2])
--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# 1st program
-print((9 ** 0.5) * 5)
+sample = 'Возвращает отцентрованную строку, по краям которой стоит символ fill (пробел по умолчанию'
+print(sample.title())
 
-# 2nd program
-print(9.99 > 9.98 and 1000 != 1000.1)
-
-# 3rd program
-firstNum = 2 * 2 + 2
-secondNum = 2 * (2 + 2)
-print(firstNum)
-print(secondNum)
-print(firstNum == secondNum)
-
-# 4th program
-numAsString = '123.456'
-numAsFloat = float(numAsString) * 10
-numAsInt = int(numAsFloat)
-print(numAsInt % 10)
