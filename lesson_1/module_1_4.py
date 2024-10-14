5c18118b81cc7e42c9d00e400585125568465e4e
Иван <cvb@sdf>
2024-10-14 23:30:15 +0300

    LESSON_1_12

---
+++ lesson_1/module_1_4.py
@@ -0,0 +1,10 @@
+#task 1
+my_string = input("Введите текст: ")
+# task 2
+print("В введенной строке: ", my_string.__len__(), " символов")
+# task 3
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
