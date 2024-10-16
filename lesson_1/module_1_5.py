7154adfec26590db7c93d8e08e31b70286093395
Иван <cvb@sdf>
2024-10-16 22:45:01 +0300

    LESSON_1_15

---
+++ lesson_1/module_1_5.py
@@ -0,0 +1,10 @@
+# task 2
+immutable_var = 1, False, "Hi"
+print(immutable_var)
+# task 3
+# immutable_var[1] = True
+# Кортежи не поддерживают изменение составляющих его эллементов.
+# task 4
+mutable_list = [12, 22.23, 'Hello']
+mutable_list[2] = False
+print(mutable_list)
