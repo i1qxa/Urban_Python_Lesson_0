0df90302d0e320b831237e11698e274f6e67beb0
Иван <cvb@sdf>
2024-11-01 00:07:17 +0300

    3_2

--- lesson_3/module_3_2.py
+++ lesson_3/module_3_2.py
@@ -1,14 +1,27 @@
-def print_params(a=1, b='строка', c=True):
-    print(a, b, c)
+def send_email(message: str, recipient: str, sender: str = "university.help@gmail.com") -> None:
+    is_email_correct: bool = True
+    list_email = [recipient, sender]
+    valid_domains = ["com", "net", "ru"]
+    for i in range(0, list_email.__len__()):
+        if not list_email[i].__contains__("@"):
+            is_email_correct = False
+            break
+        if not valid_domains.__contains__(list_email[i].split('.')[-1]):
+            is_email_correct = False
+            break
+    if not is_email_correct:
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+    if recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+        return
+    if sender != "university.help@gmail.com":
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
 
 
-print_params(b=25)
-print_params(c=[1, 2, 3])
-
-values_list = [10, False, 'Bobr']
-values_dict = {'a': True, 'b': 13, 'c': 'Пятница'}
-print_params(*values_list)
-print_params(**values_dict)
-
-values_list_2 = ['Ololo', 12.56]
-print_params(*values_list_2, 42)
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
