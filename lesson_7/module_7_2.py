e451a53333f328cc89b9e7b6e436580ce716fe4e
Иван <cvb@sdf>
2024-12-08 01:37:58 +0300

    7_2

---
+++ lesson_7/module_7_2.py
@@ -0,0 +1,27 @@
+def custom_write(file_name: str, strings: [str]):
+    file = open(file_name, 'a', encoding='utf-8')
+    pos = 1
+    res = {}
+    for string in strings:
+        res[(pos, file.tell())] = string
+        file.write(f"{string}\n")
+        pos += 1
+    return res
+
+
+info = [
+
+    'Text for tell.',
+
+    'Используйте кодировку utf-8.',
+
+    'Because there are 2 languages!',
+
+    'Спасибо!'
+
+]
+
+result = custom_write('test.txt', info)
+
+for elem in result.items():
+    print(elem)
