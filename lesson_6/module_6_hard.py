10b87d7bb09e427245b868faf98c352376f67ab3
Иван <cvb@sdf>
2024-12-07 23:10:42 +0300

    6_hard

--- lesson_6/module_6_hard.py
+++ lesson_6/module_6_hard.py
@@ -81,7 +81,6 @@ class Cube(Figure):
         super().__init__(12, colors)
 
     def get_volume(self):
-        pass
         return self.get_sides()[0] ** 3
 
     def set_sides(self, *new_sides) -> None:
@@ -105,42 +104,34 @@ class Cube(Figure):
         return self.__sides
 
 
-# circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
-#
-# cube1 = Cube((222, 35, 130), 6)
+circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
 
-triangle1 = Triangle((12,22,33), 3,4,5)
+cube1 = Cube((222, 35, 130), 6)
 
-# # Проверка на изменение цветов:
-#
-# circle1.set_color(55, 66, 77)  # Изменится
-#
-# print(circle1.get_color())
-#
-# cube1.set_color(300, 70, 15)  # Не изменится
-#
-# print(cube1.get_color())
-#
-# # Проверка на изменение сторон:
-#
-# cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
-#
-# print(cube1.get_sides())
-#
-# circle1.set_sides(15)  # Изменится
-#
-# print(circle1.get_sides())
-#
-# # Проверка периметра (круга), это и есть длина:
-#
-# print(len(circle1))
-#
-# # Проверка объёма (куба):
-#
-# print(cube1.get_volume())
+# Проверка на изменение цветов:
 
-# Проверка треугольника
+circle1.set_color(55, 66, 77)  # Изменится
 
-print(triangle1.get_sides())
-print(triangle1.get_square())
+print(circle1.get_color())
 
+cube1.set_color(300, 70, 15)  # Не изменится
+
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+
+cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
+
+print(cube1.get_sides())
+
+circle1.set_sides(15)  # Изменится
+
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+
+print(len(circle1))
+
+# Проверка объёма (куба):
+
+print(cube1.get_volume())
