b7a00a6c2cb03151142d1b1be3013da15ebf1cac
Иван <cvb@sdf>
2025-01-02 22:14:51 +0300

    9_1

--- lesson_9/module_9_1.py
+++ lesson_9/module_9_1.py
@@ -1,10 +1,13 @@
 def apply_all_func(int_list, *functions):
     result = {}
     for function in functions:
-        result[function.__name__] = function(int_list)
+        try:
+            result[function.__name__] = function(int_list)
+        except BaseException as exc:
+            print(f"Ошибка при попытке применения функции: {function.__name__} к {type(int_list)}\nОшибка: {exc.args}")
     return result
 
 
-print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], max, min, int))
 
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
